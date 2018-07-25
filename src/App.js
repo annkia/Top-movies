d29804bd8dd38fedd001449e490c67fbee0643d7
Anna <annkiaolender@gmail.com>
2018-07-25 10:55:56 +0200

    Using props to pass data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 import MovieCard from './components/MovieCard'
+//tworzymy zmienną, któa będzie przechowywała Movie object
+//chcę to przekazac do MovieCard
+//Movie card ma wyswietlac info o filmach wiec uzyjemy wlasciwosci 'movie'
+const starWars={id:1,title:'Star Wars'};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <MovieCard />
+       <MovieCard movie={starWars}/>
       </div>
     );
   }
