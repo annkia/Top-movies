2380d0a02a425ee3dd3da5ffa1ed11b6b7ecb464
Anna <annkiaolender@gmail.com>
2018-07-25 11:05:29 +0200

    Repeating components

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import MovieCard from './components/MovieCard'
 //tworzymy zmienną, któa będzie przechowywała Movie object
 //chcę to przekazac do MovieCard
 //Movie card ma wyswietlac info o filmach wiec uzyjemy wlasciwosci 'movie'
+
 const starWars={id:1,title:'Star Wars'};
+const movies=[
+    {id:1, title: 'Star Wars'},
+    {id:2, title: 'Star Trek'},
+    {id:3, title: 'Blade Runner'},
+];
+//wykorzystamy funkcje mapująca do tablic z js
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <MovieCard movie={starWars}/>
+        {movies.map(movie=> <MovieCard key={movie.id} movie={movie} />)}
       </div>
     );
   }
