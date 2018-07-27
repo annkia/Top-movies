2acda66cb8c5d083e483d103b73f5d8353000ab7
Anna <annkiaolender@gmail.com>
2018-07-27 18:20:39 +0200

    Managing state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import MovieCard from './components/MovieCard'
-//tworzymy zmienną, któa będzie przechowywała Movie object
-//chcę to przekazac do MovieCard
-//Movie card ma wyswietlac info o filmach wiec uzyjemy wlasciwosci 'movie'
+import MovieCard from './components/MovieCard';
 
-const starWars={id:1,title:'Star Wars'};
-const movies=[
-    {id:1, title: 'Star Wars'},
-    {id:2, title: 'Star Trek'},
-    {id:3, title: 'Blade Runner'},
+
+const originalMovies=[
+    {id:1, title: 'Star Wars' },
+    {id:2, title: 'Star Trek' },
+    {id:3, title: 'Blade Runner' },
 ];
-//wykorzystamy funkcje mapująca do tablic z js
 
 class App extends Component {
+ state = { movies:[] };
+
+    componentDidMount(){
+    setTimeout(()=>
+        this.setState({movies: originalMovies}), 5000);
+               
+    }
+
   render() {
+      const {movies}=this.state;
+      
     return (
       <div className="App">
         {movies.map(movie=> <MovieCard key={movie.id} movie={movie} />)}
