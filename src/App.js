067ad58b6ec472bf9e377d09d6349e293f3f5760
Anna <annkiaolender@gmail.com>
2018-07-27 19:10:51 +0200

    Loading list of movies using AJAX

--- src/App.js
+++ src/App.js
@@ -13,10 +13,12 @@ const originalMovies=[
 class App extends Component {
  state = { movies:[] };
 
-    componentDidMount(){
-    setTimeout(()=>
-        this.setState({movies: originalMovies}), 5000);
-               
+    async componentDidMount(){
+   // setTimeout(()=>
+    //    this.setState({movies: originalMovies}), 5000);
+    const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=41b53f3aecb5d28360372cf8a29df0fa&language=en-US&page=1');   
+    const json = await response.json();
+        this.setState({movies:json.results});
     }
 
   render() {
